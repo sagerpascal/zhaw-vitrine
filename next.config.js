1a1c008eda39048c98c4d25e48de19b1b9f2436e
sagerpascal <paes.sager@gmail.com>
2023-12-20 22:27:30 +0100

    Slides created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/',
+                destination: '/html/presentation.html',
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
